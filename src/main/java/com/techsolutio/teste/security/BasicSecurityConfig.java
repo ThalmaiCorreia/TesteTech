a5a16ab5d5f5f3cda152d4786a2f7fa89b9a86b3
Thalmai Correia <thalmaicorreia7@gmail.com>
2022-09-13 16:12:37 -0300

    Configuração da camada de Segurança.

--- src/main/java/com/techsolutio/teste/security/BasicSecurityConfig.java
+++ src/main/java/com/techsolutio/teste/security/BasicSecurityConfig.java
@@ -2,52 +2,67 @@ package com.techsolutio.teste.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
-
+@Configuration
 @EnableWebSecurity
-public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class BasicSecurityConfig {
+
+    private AuthenticationManager authManager;
+
+    private AuthenticationManagerBuilder auth;
 
     @Autowired
-    private UserDetailsService userDetailsService;
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-        auth.userDetailsService(userDetailsService);
-
-        auth.inMemoryAuthentication()
-                .withUser("root")
-                .password(passwordEncoder().encode("root"))
-                .authorities("ROLE_USER");
-
-    }
+    UserDetailsService userDetailsService;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.authorizeRequests()
-                .antMatchers("/usuarios/logar").permitAll()
-                .antMatchers("/usuarios/cadastrar").permitAll()
-                .antMatchers(HttpMethod.OPTIONS).permitAll()
-                .anyRequest().authenticated()
-                .and().httpBasic()
-                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().cors()
-                .and().csrf().disable();
+        auth = http.getSharedObject(AuthenticationManagerBuilder.class);
+
+        auth.userDetailsService(userDetailsService);
+
+        auth.inMemoryAuthentication()
+                .withUser("root")
+                .password(passwordEncoder().encode("root"))
+                .roles("USER");
+
+        authManager = auth.build();
+
+        http
+                .authenticationManager(authManager)
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().csrf().disable()
+                .cors();
+
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .antMatchers("/usuarios/logar").permitAll()
+                        .antMatchers("/usuarios/cadastrar").permitAll()
+                        .antMatchers(HttpMethod.OPTIONS).permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic();
+
+        return http.build();
 
     }
+
 }
